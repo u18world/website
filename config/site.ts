f3ad27b9fc7d5a3178ca2496bd6b92f0172722ab
Leno <87892177+lennoncoder@users.noreply.github.com>
2024-05-24 20:46:21 +0530

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -4,12 +4,6 @@ export const siteConfig = {
   name: "Under18 Elections",
   description:
     "Beautifully designed website for the under 18 Elections.",
-  mainNav: [
-    {
-      title: "Home",
-      href: "/",
-    },
-  ],
   links: {
     twitter: "",
     instagram: "",
