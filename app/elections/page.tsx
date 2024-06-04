6e06f8cec2107ffb08c671ceac9b05f3a28a94de
Leno <87892177+lennoncoder@users.noreply.github.com>
2024-06-04 13:08:19 +0000

    add details

--- app/elections/page.tsx
+++ app/elections/page.tsx
@@ -6,3 +6,14 @@ export const metadata = {
 export default function Elections() {
   return <div>Elections</div>;
 }
+
+/**
+Name:
+Class:
+School Name:
+Phone Number:
+Email Id:
+Start Date: Calendar option
+Program Name: Kratos Mini Internship; Business Rockstar, Kratos Mini Internship: Psychology and Finance
+// Shadow-ship: Entrepreneurship, Law, Movie making, Software engineering, Automobile Engineering, Psychology, Medicine
+ */
