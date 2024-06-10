ac04ef8d52dab36526e8f43fd854296966d50c0b
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-06-10 11:25:18 +0000

    added perms to admin

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,38 +1,26 @@
 "use client";
 
-import { admin } from "@/actions/admin";
+import { PrismaClient } from "@prisma/client";
 import { RoleGate } from "@/components/auth/role-gate";
 import { FormSuccess } from "@/components/form-success";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { UserRole } from "@prisma/client";
 import { toast } from "sonner";
+const prisma = new PrismaClient();
 
-const AdminPage = () => {
-  const onServerActionClick = () => {
-    admin()
-      .then((data) => {
-        if (data.error) {
-          toast.error(data.error);
-        }
+export default async function AdminPage() {
+    const id = "clx8vj5mi0000m2i8z5ku2y3f";
 
-        if (data.success) {
-          toast.success(data.success);
-        }
-      })
-  }
-  
-  const onApiRouteClick = () => {
-    fetch("/api/admin")
-      .then((response) => {
-        if (response.ok) {
-          toast.success("Allowed API Route!");
-        } else {
-          toast.error("Forbidden API Route!");
-        }
-      })
-  }
-
+    async function Update() {
+      await prisma.user.update({
+         where: { id: id },
+         data: {
+           score: 2,
+         },
+       });
+    }
+   
   return (
     <Card className="w-[600px]">
       <CardHeader>
@@ -50,22 +38,11 @@ const AdminPage = () => {
           <p className="text-sm font-medium">
             Admin-only API Route
           </p>
-          <Button onClick={onApiRouteClick}>
-            Click to test
-          </Button>
-        </div>
-
-        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
-          <p className="text-sm font-medium">
-            Admin-only Server Action
-          </p>
-          <Button onClick={onServerActionClick}>
-            Click to test
+          <Button onClick={Update}>
+            set score to 2
           </Button>
         </div>
       </CardContent>
     </Card>
   );
 };
-
-export default AdminPage;
