c1a0e6e8051384f025f06b50aed9e28b5a5f1845
Avalynn <avalynndev@gmail.com>
2024-06-10 10:07:04 +0530

    update layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,13 +1,11 @@
-const AuthLayout = ({ 
-  children
-}: { 
-  children: React.ReactNode
-}) => {
-  return ( 
-    <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-      {children}
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="pt-24 pb-24 flex items-center justify-center antialiased dark:bg-grid-white/[0.02] bg-grid-black/[0.02] relative overflow-hidden">
+      <div className="dark:border-white/[0.2] border border-transparent max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+        {children}
+      </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default AuthLayout;
