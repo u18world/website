7ccc717e5241b9598987f3d62b3bd2517a21bc61
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-06-10 08:46:31 +0000

    fix ui

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -2,15 +2,15 @@ import { Navbar } from "./_components/navbar";
 
 interface ProtectedLayoutProps {
   children: React.ReactNode;
-};
+}
 
 const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
   return (
-    <div className="pt-24 pb-24 flex items-center justify-center antialiased dark:bg-grid-white/[0.02] bg-grid-black/[0.02] relative overflow-hidden">
+    <div className="pt-24 pb-24 flex flex-col gap-y-10 items-center justify-center antialiased dark:bg-grid-white/[0.02] bg-grid-black/[0.02] relative overflow-hidden">
       <Navbar />
       {children}
     </div>
   );
-}
- 
+};
+
 export default ProtectedLayout;
