d5b996fb3c900910642c73fb4b1dd9c1b71413e6
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-06-10 07:14:52 +0000

    navbar login

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,7 +20,7 @@ import {
 } from "@/components/ui/navigation-menu";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import Image from "next/image";
+import { LoginButton } from "@/components/auth/login-button";
 import { ThemeToggle } from "@/components/theme-toggle";
 
 interface RouteProps {
@@ -87,7 +87,7 @@ export const Navbar = () => {
   return (
     <header className="shadow-inner bg-opacity-15 w-[90%] md:w-[70%] lg:w-[75%] lg:max-w-screen-xl top-5 mx-auto sticky border border-secondary z-40 rounded-2xl flex justify-between items-center p-2 bg-card">
       <Link href="/" className="font-bold text-lg flex items-center">
-        <Earth className="w-9 h-9 mr-2"/>
+        <Earth className="w-9 h-9 mr-2" />
         Under18
       </Link>
       {/* <!-- Mobile --> */}
@@ -183,6 +183,15 @@ export const Navbar = () => {
                   </Link>
                 </NavigationMenuLink>
               ))}
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+                <NavigationMenuLink key={"login"} asChild>
+                  <LoginButton asChild>
+                    <Button variant="secondary" size="lg">
+                      Sign in
+                    </Button>
+                  </LoginButton>
+                </NavigationMenuLink>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
