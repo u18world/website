2cfbe7ae13e427bebde501e91c7b6f0abb6590dd
Avalynn <avalynndev@gmail.com>
2024-06-10 10:55:09 +0530

    remove fill

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -87,7 +87,7 @@ export const Navbar = () => {
   return (
     <header className="shadow-inner bg-opacity-15 w-[90%] md:w-[70%] lg:w-[75%] lg:max-w-screen-xl top-5 mx-auto sticky border border-secondary z-40 rounded-2xl flex justify-between items-center p-2 bg-card">
       <Link href="/" className="font-bold text-lg flex items-center">
-        <Earth className="w-9 h-9 mr-2" fill="currentColor" />
+        <Earth className="w-9 h-9 mr-2"/>
         Under18
       </Link>
       {/* <!-- Mobile --> */}
@@ -108,7 +108,7 @@ export const Navbar = () => {
               <SheetHeader className="mb-4 ml-4">
                 <SheetTitle className="flex items-center">
                   <Link href="/" className="flex items-center">
-                    <Earth className="w-9 h-9 mr-2" fill="currentColor" />
+                    <Earth className="w-9 h-9 mr-2" />
                     Under18
                   </Link>
                 </SheetTitle>
