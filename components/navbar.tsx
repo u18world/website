7387c4a05dd9e713772cd4fceed56fae7b54bfef
Avalynn <avalynndev@gmail.com>
2024-06-10 10:34:16 +0530

    update navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ChevronsDown, Github, Menu } from "lucide-react";
+import { Earth, Github, Menu } from "lucide-react";
 import React from "react";
 import {
   Sheet,
@@ -35,20 +35,32 @@ interface FeatureProps {
 
 const routeList: RouteProps[] = [
   {
-    href: "#testimonials",
-    label: "Testimonials",
+    href: "/about",
+    label: "About",
   },
   {
-    href: "#team",
-    label: "Team",
+    href: "/blog",
+    label: "Blog",
+  },
+  {
+    href: "/careers",
+    label: "Careers",
+  },
+  {
+    href: "/superheros",
+    label: "Auperheros",
+  },
+  {
+    href: "/elections",
+    label: "Elections",
   },
   {
     href: "#contact",
     label: "Contact",
   },
   {
-    href: "#faq",
-    label: "FAQ",
+    href: "#leaders",
+    label: "Leaders",
   },
 ];
 
@@ -75,8 +87,8 @@ export const Navbar = () => {
   return (
     <header className="shadow-inner bg-opacity-15 w-[90%] md:w-[70%] lg:w-[75%] lg:max-w-screen-xl top-5 mx-auto sticky border border-secondary z-40 rounded-2xl flex justify-between items-center p-2 bg-card">
       <Link href="/" className="font-bold text-lg flex items-center">
-        <ChevronsDown className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
-        Shadcn
+        <Earth className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
+        U18 World
       </Link>
       {/* <!-- Mobile --> */}
       <div className="flex items-center lg:hidden">
@@ -96,8 +108,8 @@ export const Navbar = () => {
               <SheetHeader className="mb-4 ml-4">
                 <SheetTitle className="flex items-center">
                   <Link href="/" className="flex items-center">
-                    <ChevronsDown className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
-                    Shadcn
+                    <Earth className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
+                    U18 World
                   </Link>
                 </SheetTitle>
               </SheetHeader>
@@ -119,7 +131,6 @@ export const Navbar = () => {
 
             <SheetFooter className="flex-col sm:flex-col justify-start items-start">
               <Separator className="mb-2" />
-
               <ThemeToggle />
             </SheetFooter>
           </SheetContent>
@@ -127,52 +138,54 @@ export const Navbar = () => {
       </div>
 
       {/* <!-- Desktop --> */}
-       <div className="hidden lg:flex lg:w-full lg:justify-center">
+      <div className="hidden lg:flex lg:w-full lg:justify-center">
         <NavigationMenu className="flex justify-center">
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger className="bg-card text-base">
-              Features
-            </NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <div className="grid w-[600px] grid-cols-2 gap-5 p-4">
-                <Image
-                  src=""
-                  alt="RadixLogo"
-                  className="h-full w-full rounded-md object-cover"
-                  width={600}
-                  height={600}
-                />
-                <ul className="flex flex-col gap-2">
-                  {featureList.map(({ title, description }) => (
-                    <li
-                      key={title}
-                      className="rounded-md p-3 text-sm hover:bg-muted"
-                    >
-                      <p className="mb-1 font-semibold leading-none text-foreground">
-                        {title}
-                      </p>
-                      <p className="line-clamp-2 text-muted-foreground">
-                        {description}
-                      </p>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
+          <NavigationMenuList>
+            {/** 
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className="bg-card text-base">
+                Features
+              </NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <div className="grid w-[600px] grid-cols-2 gap-5 p-4">
+                  <Image
+                    src=""
+                    alt="RadixLogo"
+                    className="h-full w-full rounded-md object-cover"
+                    width={600}
+                    height={600}
+                  />
+                  <ul className="flex flex-col gap-2">
+                    {featureList.map(({ title, description }) => (
+                      <li
+                        key={title}
+                        className="rounded-md p-3 text-sm hover:bg-muted"
+                      >
+                        <p className="mb-1 font-semibold leading-none text-foreground">
+                          {title}
+                        </p>
+                        <p className="line-clamp-2 text-muted-foreground">
+                          {description}
+                        </p>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            */}
 
-          <NavigationMenuItem>
-            {routeList.map(({ href, label }) => (
-              <NavigationMenuLink key={href} asChild>
-                <Link href={href} className="justify-start text-base px-2">
-                  {label}
-                </Link>
-              </NavigationMenuLink>
-            ))}
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+            <NavigationMenuItem>
+              {routeList.map(({ href, label }) => (
+                <NavigationMenuLink key={href} asChild>
+                  <Link href={href} className="justify-start text-base px-2">
+                    {label}
+                  </Link>
+                </NavigationMenuLink>
+              ))}
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
       </div>
 
       <div className="hidden lg:flex">
