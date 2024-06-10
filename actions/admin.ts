ea397218b6d46e8a8158f828a48fe4b88acf01c1
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-06-10 12:01:54 +0000

    change value

--- actions/admin.ts
+++ actions/admin.ts
@@ -5,14 +5,14 @@ import { UserRole } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const admin = async () => {
+export const admin = async ({number}:any) => {
   const role = await currentRole();
 
   if (role === UserRole.ADMIN) {
     await prisma.user.update({
       where: { id: "clx8vj5mi0000m2i8z5ku2y3f" },
       data: {
-        score: 2,
+        score: number,
       },
     });
     return { success: "Allowed Server Action!" };
--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -6,21 +6,22 @@ import { FormSuccess } from "@/components/form-success";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { UserRole } from "@prisma/client";
+import { useState } from "react";
 import { toast } from "sonner";
 
 const AdminPage = () => {
+  const [number,setNumber] = useState("")
   const onServerActionClick = () => {
-    admin()
-      .then((data) => {
-        if (data.error) {
-          toast.error(data.error);
-        }
+    admin(number).then((data) => {
+      if (data.error) {
+        toast.error(data.error);
+      }
 
-        if (data.success) {
-          toast.success(data.success);
-        }
-      })
-  }
+      if (data.success) {
+        toast.success(data.success);
+      }
+    });
+  };
   
   const onApiRouteClick = () => {
     fetch("/api/admin")
@@ -36,32 +37,25 @@ const AdminPage = () => {
   return (
     <Card className="w-[600px]">
       <CardHeader>
-        <p className="text-2xl font-semibold text-center">
-          🔑 Admin
-        </p>
+        <p className="text-2xl font-semibold text-center">🔑 Admin</p>
       </CardHeader>
       <CardContent className="space-y-4">
         <RoleGate allowedRole={UserRole.ADMIN}>
-          <FormSuccess
-            message="You are allowed to see this content!"
-          />
+          <FormSuccess message="You are allowed to see this content!" />
         </RoleGate>
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
-          <p className="text-sm font-medium">
-            Admin-only API Route
-          </p>
-          <Button onClick={onApiRouteClick}>
-            Click to test
-          </Button>
+          <p className="text-sm font-medium">Admin-only API Route</p>
+          <Button onClick={onApiRouteClick}>Click to test</Button>
         </div>
 
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
-          <p className="text-sm font-medium">
-            Admin-only Server Action
-          </p>
-          <Button onClick={onServerActionClick}>
-            Click to test
-          </Button>
+          <p className="text-sm font-medium">Admin-only Server Action</p>
+          <input
+            type="text"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+          />
+          <Button onClick={onServerActionClick}>Click to test</Button>
         </div>
       </CardContent>
     </Card>
