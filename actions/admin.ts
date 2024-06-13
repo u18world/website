e1c455bd1df97dfa0b6bb4dae74d53da0ec4d686
Leno <87892177+lennoncoder@users.noreply.github.com>
2024-06-13 13:34:54 +0530

    Update admin.ts

--- actions/admin.ts
+++ actions/admin.ts
@@ -5,17 +5,17 @@ import { UserRole } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const admin = async ({number}:any) => {
+export const admin = async () => {
   const role = await currentRole();
 
   if (role === UserRole.ADMIN) {
     await prisma.user.update({
       where: { id: "clxcyviqm0000ysaa7inicp57" },
       data: {
-        total_score: number,
+        total_score: 7,
       },
     });
-    return { success: "Allowed Server Action!" };
+    return { success: "Ran Action!" };
   }
 
   return { error: "Forbidden Server Action!" }
